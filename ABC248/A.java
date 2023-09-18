56459418ed7b7e1fe013d6a29122fe63492476c3
caf112 <yuu10san.14@gmail.com>
2023-09-18 23:26:45 +0900

    add

---
+++ ABC248/A.java
@@ -0,0 +1,28 @@
+package ABC248;
+
+import java.util.*;
+
+public class A {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        String S = scanner.nextLine();
+        boolean[] flag = new boolean[10];
+
+        for(int i = 0;i<10;i++){
+            flag[i] = true;
+        }
+
+        for(int i = 0;i<9;i++){
+            flag[Character.getNumericValue(S.charAt(i))] = false;
+        }
+
+        for(int i = 0;i<10;i++){
+            if(flag[i]){
+                System.out.println(i);
+            }
+        }
+
+        scanner.close();
+    }
+}
